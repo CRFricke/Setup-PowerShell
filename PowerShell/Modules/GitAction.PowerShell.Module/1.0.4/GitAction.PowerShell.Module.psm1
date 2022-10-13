6c5ab1c1df9cb2edf9b7767e356737007ee84e8a
Chuck Fricke <80228320+CRFricke@users.noreply.github.com>
2022-10-13 00:28:49 -0400

    Use last repository tag to generate version variables for checkins. Use prior repository tag to validate new tags.

--- PowerShell/Modules/GitAction.PowerShell.Module/1.0.4/GitAction.PowerShell.Module.psm1
+++ PowerShell/Modules/GitAction.PowerShell.Module/1.0.4/GitAction.PowerShell.Module.psm1
@@ -3,113 +3,145 @@
 ## Portions stollen from:
 ##    https://github.com/MADE-Apps/MADE.NET/blob/main/build/GetBuildVersion.psm1
 
-## Parses a $VersionString parameter of the form:
-##   'refs/tags/v6.0.1-beta1.0'
-##   'refs/tags/v6.0.1'
-##
-## If $VersionString does not start with "refs/tags/",
-## the VERSION_DEFAULT environment variable is parsed instead.
-
 function Get-VersionVariables {
     [CmdletBinding()]
-    Param (
-      [Parameter(Position = 0, Mandatory)]
-      [string]$VersionString
-    )
 
-    Write-Host "`$VersionString: '$VersionString'"
+    Write-Host "GITHUB_REF: $env:GITHUB_REF"
 
-    $graphResult = gh api graphql -F owner='{owner}' -F name='{repo}' -f query=' 
-query($name: String!, $owner: String!) {
+# For GITHUB_REF_TYPE 'tag', the old tag is the 2nd to last one.
+$count = ($env:GITHUB_REF_TYPE -eq 'tag') ? 2 : 1
+
+$graphResult = gh api graphql -F owner='CRFricke' -F name='Authorization.Core' -F count=$count -f query=' 
+query($name: String!, $owner: String! $count: Int!) {
   repository(owner: $owner, name: $name) {
-    refs(refPrefix: \"refs/tags/\", last: 1) {
-      nodes { name }
-    }
+	refs(refPrefix: \"refs/tags/\", last: $count) {
+	  nodes { name }
+	}
   }
 }'
 
-    Write-Host "GraphQL result: $graphResult"
+$oldTag = ($graphResult | ConvertFrom-Json).data.repository.refs.nodes[0].name
+if ($oldTag)
+{
+	Write-Host "Repository tag version: $oldTag"
 
-    # Parse via regex
-    $null = $env:VERSION_DEFAULT -match '(?<major>\d+)(\.(?<minor>\d+))?(\.(?<patch>\d+))?(\-(?<pre>[0-9A-Za-z\-\.]+))?'
+	$null = $oldTag -match '(?<major>\d+)(\.(?<minor>\d+))?(\.(?<patch>\d+))?(\-(?<pre>[0-9A-Za-z\-\.]+))?'
 	if (!$matches)
 	{
-		throw "Invalid VERSION_DEFAULT environment variable value: '$env:VERSION_DEFAULT'."
+		throw "Invalid tag value found in repository: '$oldTag'."
 	}
-	
-	$version_default = @{
-	    Major = [uint64]$matches['major']
+
+	$oldTagVersion = @{
+		Major = [uint64]$matches['major']
+		Minor = [uint64]$matches['minor']
+		Patch = [uint64]$matches['patch']
+		PreRelease = $matches['pre']
+		Build = $env:GITHUB_RUN_NUMBER
+	}
+
+	if (!$oldTagVersion.PreRelease)
+	{
+		$oldTagVersion.PreRelease = 'build'
+	}
+}
+else
+{
+	$oldTagVersion = @{
+		Major = 1
+		Minor = 0
+		Patch = 0
+		PreRelease = 'alpha1.0'
+		Build = $env:GITHUB_RUN_NUMBER
+	}
+}
+
+if ($env:GITHUB_REF_TYPE -eq 'tag')
+{
+	$null = $env:GITHUB_REF -match '(?<major>\d+)(\.(?<minor>\d+))?(\.(?<patch>\d+))?(\-(?<pre>[0-9A-Za-z\-\.]+))?(\+(?<build>[0-9A-Za-z\-\.]+))?'
+	if (!$matches)
+	{
+		throw "Invalid tag value found in GITHUB_REF value: $env:GITHUB_REF."
+	}
+
+	$tagVersion = @{
+		Major = [uint64]$matches['major']
 		Minor = [uint64]$matches['minor']
 		Patch = [uint64]$matches['patch']
 		PreRelease = [string]$matches['pre']
+		Build = [string]$matches['build']
 	}
 
-    if (-not $version_default.PreRelease)
-    {
-        $version_default.PreRelease = 'build'
-    }
-	
-	$matches = $null
+	$newTag = 'v' + $tagVersion.Major + '.' + $tagVersion.Minor + '.' + $tagVersion.Patch 
+	if ($tagVersion.PreRelease)
+	{
+		$newTag += '-' + $tagVersion.PreRelease
+	}
+	if ($tagVersion.Build)
+	{
+		$newTag += '+' + $tagVersion.Build
+	}
 
-    if ($env:GITHUB_REF_TYPE -eq 'tag')
-    {
-        $null = $VersionString -match '(?<major>\d+)(\.(?<minor>\d+))?(\.(?<patch>\d+))?(\-(?<pre>[0-9A-Za-z\-\.]+))?(\+(?<build>[0-9A-Za-z\-\.]+))?'
-	    if (!$matches)
-	    {
-		    throw 'Could not parse a valid version value from the specified $VersionString parameter.'
-	    }
-    }
+	$verOld = New-Object -TypeName System.Version -ArgumentList (
+		$oldTagVersion.Major, $oldTagVersion.Minor, $oldTagVersion.Patch
+		)
 
-    if (!$matches)
-    {
-        $Major = $version_default.Major
-        $Minor = $version_default.Minor
-        $Patch = $version_default.Patch
-        $PreRelease = $version_default.PreRelease
-    }
-    else
-    {
-        $Major = [uint64]$matches['major']
-        $Minor = [uint64]$matches['minor']
-        $Patch = [uint64]$matches['patch']
-        $PreRelease = [string]$matches['pre']
-        $Build = [string]$matches['build']
-    }
+	$verNew = New-Object -TypeName System.Version -ArgumentList (
+		$tagVersion.Major, $tagVersion.Minor, $tagVersion.Patch
+		)
 
-    if ($PreRelease -and !$Build)
-    {
-        $Build = [string]$env:GITHUB_RUN_NUMBER
-    }
+	if ($verOld.CompareTo($verNew) -gt 0 -or ($verOld.CompareTo($verNew) -eq 0 -and $oldTagVersion.PreRelease -gt $tagVersion.PreRelease))
+	{
+		throw "Error: repository tag version ($oldTag) is greater than new tag version ($newTag)."
+	}
+}
+else
+{
+	$tagVersion = $oldTagVersion
 
-    if ($env:GITHUB_REF_TYPE -eq 'tag')
-    {
-        If ( ($Major -ne $version_default.Major) -or ($Minor -ne $version_default.Minor) -or ($Patch -ne $version_default.Patch) )
-        {
-            throw "Specified Git tag does not match VERSION_DEFAULT environment variable ($env:VERSION_DEFAULT). Are you pushing to correct branch?"
-        }
-    }
+	$newTag = 'v' + $tagVersion.Major + '.' + $tagVersion.Minor + '.' + $tagVersion.Patch 
+	if ($tagVersion.PreRelease)
+	{
+		$newTag += '-' + $tagVersion.PreRelease
+	}
+	if ($tagVersion.Build)
+	{
+		$newTag += '+' + $tagVersion.Build
+	}
+}
 
-    Enter-ActionOutputGroup "Dump Output Variables"
+Enter-ActionOutputGroup "Dump Output Variables"
 
-    Write-Host "`$Tag_Major: $Major"
-    $env:TAG_MAJOR = $Major
-    $global:Tag_Major = $Major
+$env:TAG_MAJOR = $tagVersion.Major
+$global:Tag_Major = $tagVersion.Major
+Write-Host "`$Tag_Major: $Tag_Major"
 
-    Write-Host "`$Tag_Minor: $Minor"
-    $env:TAG_MINOR = $Minor
-    $global:Tag_Minor = $Minor
+$env:TAG_MINOR = $tagVersion.Minor
+$global:Tag_Minor = $tagVersion.Minor
+Write-Host "`$Tag_Minor: $Tag_Minor"
 
-    Write-Host "`$Tag_Patch: $Patch"
-    $env:TAG_PATCH = $Patch
-    $global:Tag_Patch = $Patch
+$env:TAG_PATCH = $tagVersion.Patch
+$global:Tag_Patch = $tagVersion.Patch
+Write-Host "`$Tag_Patch: $Tag_Patch"
 
-    Write-Host "`$Tag_PreRelease: $PreRelease"
-    $env:TAG_PRERELEASE = $PreRelease
-    $global:Tag_PreRelease = $PreRelease
+$env:TAG_PRERELEASE = $tagVersion.PreRelease
+$global:Tag_PreRelease = $tagVersion.PreRelease
+Write-Host "`$Tag_PreRelease: $Tag_PreRelease"
 
-    Write-Host "`$Tag_Build: $Build"
-    $env:TAG_BUILD = $Build
-    $global:Tag_Build = $Build
+$env:TAG_BUILD = $tagVersion.Build
+$global:Tag_Build = $tagVersion.Build
+Write-Host "`$Tag_Build: $Tag_Build"
 
-    Exit-ActionOutputGroup
+$env:TAG_VERSION = $newTag
+$global:Tag_Version = $newTag
+Write-Host "`$Tag_Version: $Tag_Version"
+
+Exit-ActionOutputGroup
+
+Enter-ActionOutputGroup "Dump GitHub Variables"
+
+Write-Host "GITHUB_REF: $env:GITHUB_REF"
+Write-Host "GITHUB_REF_TYPE: $env:GITHUB_REF_TYPE"
+Write-Host "GITHUB_RUN_NUMBER: $env:GITHUB_RUN_NUMBER"
+
+Exit-ActionOutputGroup
 }
